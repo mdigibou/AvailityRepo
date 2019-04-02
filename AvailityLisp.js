647e46310fbaa1734bb6a1bc05ff756d03aa9515
mdigibou <mdigibou@yahoo.com>
2019-04-01 21:42:13 -0400

    Add files via upload

---
+++ AvailityLisp.js
@@ -0,0 +1,25 @@
+var newThing = function(x)
+{ 
+	if(typeof x !== "string") 
+		return false; 
+	var stringArray = x.split("");
+	var openCounter = 0;
+	for(var q = 0; q< stringArray.length; q++)
+	{
+		if(stringArray[q] == '(')
+        {			
+			openCounter++;			
+        }
+		else if (stringArray[q] == ')')
+        {			
+			if(openCounter <= 0)
+				return false;
+			else
+				openCounter--;
+        }			
+    }
+	if(openCounter == 0)
+		return true;
+	else
+		return false;	
+}
